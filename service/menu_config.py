9004e3f133bc6fbc04866f78a6a4534fbcec61f3
huok <77536370+dee-shar@users.noreply.github.com>
2025-08-07 18:01:53 +0500

    Create menu_config.py

---
+++ service/menu_config.py
@@ -0,0 +1,34 @@
+table_style = "psql"
+
+menu = [
+	[1, "Spam messages"],
+	[2, "Chat tools"],
+	[3, "Activity tools"],
+	[4, "Profile tools"],
+	[5, "Raiding tools"],
+	[6, "Exit"]
+]
+
+chat_tools = [
+	[1, "Retrieve chat identification number"],
+	[2, "Transfer fake amount of coins"],
+	[3, "Spam with system messages"],
+	[4, "Spam chat notifications with join & leave"]
+]
+
+activity_tools = [
+	[1, "Invite users into the chat"],
+	[2, "Like recent posted blogs"],
+	[3, "Follow online users"],
+	[4, "Delete all followings"]
+]
+
+profile_tools = [
+	[1, "Spam by creating multiple blogs"],
+	[2, "Spam by creating multiple wikis"]
+]
+
+retrieve_chat_id_submenu = [
+	[1, "Get public chats identification numbers"],
+	[2, "Get joined chats identification numbers"]
+]
