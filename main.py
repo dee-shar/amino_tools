5d5f60b36e7398ffd4be94679d81f474c894fc00
huok <77536370+dee-shar@users.noreply.github.com>
2025-11-12 20:08:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from src import tools
 from asyncio import get_event_loop
 
-print("""
-▄▀█ █▀▄▀█ █ █▄░█ █▀█ ▄▄ ▀█▀ █▀█ █▀█ █░░ █▀
-█▀█ █░▀░█ █ █░▀█ █▄█ ░░ ░█░ █▄█ █▄█ █▄▄ ▄█
-""")
-
 get_event_loop().run_until_complete(tools.run_service())
